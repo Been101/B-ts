cd62d45100b440aa8add77e4dbc24372e0ed49a3
been <1043609130@qq.com>
2019-10-12 12:30:20 +0800

    ts classes type

--- index.ts
+++ index.ts
@@ -448,20 +448,71 @@ const grid = new Grid()
 // 抽象类中的抽象方法不包含具体实现并且必须在派生类中实现
 // 抽象方法的语法与接口方法相似， 两者都是定义方法签名但不包含方法体
 
+// 函数类型的 接口
+interface Ko {
+    (name: string): number
+}
 
+const l: Ko = function (name: string): number {
+    return 5
+}
 
+abstract class Kko {
+    abstract getName(): string
+}
 
+// 派生类 必须实现 抽象类的 抽象方法
+class Koo extends Kko {
+    getName() {
+        return ''
+    }
+}
 
+// 构造函数
 
+class Greater {
+    great() { }
+}
 
+let greater: Greater // 意思是 Greater类的实例的类型是 Greater
+greater = new Greater()
 
+class Greeter {
+    static standardGreeting = "Hello, there";
+    greeting: string;
+    greet() {
+        if (this.greeting) {
+            return "Hello, " + this.greeting;
+        }
+        else {
+            return Greeter.standardGreeting;
+        }
+    }
+}
 
+let greeter1: Greeter;
+greeter1 = new Greeter();
+console.log(greeter1.greeting, 'greeter======')
+console.log(greeter1.greet());  //  'Hello there'
 
+let greeterMaker: typeof Greeter = Greeter; // 意思是取Greeter类的类型
+greeterMaker.standardGreeting = "Hey there!";
+// console.log(greeterMaker.greet()) //错。  类类型只有静态属性和构造函数
 
+let greeter2: Greeter = new greeterMaker();
+console.log(greeter2.greet()); // Hey there
 
+// 类也可以作为接口来使用
+class Points {
+    x: number;
+    y: number;
+}
 
+interface Point3d extends Points {
+    z: number;
+}
 
-
+let point3d: Point3d = { x: 1, y: 2, z: 3 };
 
 
 

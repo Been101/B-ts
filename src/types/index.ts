ef866f30030df61e830ad3acb3f5f4cd3aa0bd61
been <1043609130@qq.com>
2019-10-26 22:58:28 +0800

    add response interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -20,4 +20,18 @@ export interface AxiosRequireConfig {
   data?: any
   params?: any
   headers?: any
+  responseType?: XMLHttpRequestResponseType
+}
+
+export interface AxiosResponse {
+  data: any
+  status: number
+  statusText: string
+  headers: any
+  config: AxiosRequireConfig
+  request: any
+}
+
+export interface AxiosPromise extends Promise<AxiosResponse> {
+
 }

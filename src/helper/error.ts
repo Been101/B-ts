117c3a6821f4ae0c20171aa4405969439daf6d2d
been <1043609130@qq.com>
2019-10-28 22:38:47 +0800

    AxiosError class and error factory

---
+++ src/helper/error.ts
@@ -0,0 +1,35 @@
+import { AxiosRequireConfig, AxiosResponse } from './../types/index';
+class AxiosError extends Error {
+  isAxiosError: boolean
+  code?: string | null
+  request?: any
+  config: AxiosRequireConfig
+  response?: AxiosResponse
+  constructor(
+    message: string,
+    config: AxiosRequireConfig,
+    code?: string | null,
+    request?: any,
+    response?: AxiosResponse
+  ) {
+    super(message)
+    this.config = config
+    this.code = code
+    this.request = request
+    this.response = response
+    this.isAxiosError = true
+
+    Object.setPrototypeOf(this, AxiosError.prototype) // 兼容处理， 继承内置的类时像 Error, Array, Map， 不能调用方法或 instenceof will return false 的bug
+  }
+}
+
+export function createError(
+  message: string,
+  config: AxiosRequireConfig,
+  code?: string | null,
+  request?: any,
+  response?: AxiosResponse
+) {
+  const error = new AxiosError(message, config, code, request, response)
+  return error
+}

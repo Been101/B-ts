3c9f4afc2696602db1d294efbda40db1cecaf874
been <1043609130@qq.com>
2019-10-28 23:14:50 +0800

    add ts-loader

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -3,10 +3,22 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
-  entry: './example/apps/base.js',
+  entry: './example/apps/base.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
   },
   plugins: [
     new HtmlWebpackPlugin({

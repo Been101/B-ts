5040ece67c654247583a9e20eba32f03fe407c7e
been <1043609130@qq.com>
2019-10-31 22:36:27 +0800

    add request method

--- src/core/Axios.ts
+++ src/core/Axios.ts
@@ -6,15 +6,41 @@ export default class Axios {
     return dispatchRequest(config)
   }
   get(url: string, config: AxiosRequireConfig): AxiosPromise {
+    return this._requestMethodWithoutData('get', url, config)
+  }
+
+  delete(url: string, config: AxiosRequireConfig): AxiosPromise {
+    return this._requestMethodWithoutData('delete', url, config)
+  }
+
+  head(url: string, config: AxiosRequireConfig): AxiosPromise {
+    return this._requestMethodWithoutData('head', url, config)
+  }
+
+  options(url: string, config: AxiosRequireConfig): AxiosPromise {
+    return this._requestMethodWithoutData('options', url, config)
+  }
+
+  post(url: string, data?: any, config?: AxiosRequireConfig): AxiosPromise {
+    return this._requestMethodWithData('options', url, data, config)
+  }
+
+  patch(url: string, data?: any, config?: AxiosRequireConfig): AxiosPromise {
+    return this._requestMethodWithData('patch', url, data, config)
+  }
+
+  _requestMethodWithoutData(method: string, url: string, config?: AxiosRequireConfig) {
     return this.request(Object.assign(config || {}, {
-      method: 'get',
+      method,
       url
     }))
   }
-  delete(url: string, config: AxiosRequireConfig): AxiosPromise {
+
+  _requestMethodWithData(method: string, url: string, data?: any, config?: AxiosRequireConfig) {
     return this.request(Object.assign(config || {}, {
-      method: 'delete',
-      url
+      method,
+      url,
+      data
     }))
   }
 
